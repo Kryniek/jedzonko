e1733b1b4489bdef7534f387ceb054dcd40bf171
Krystian Szatkiewicz <shasu93@gmail.com>
2020-05-01 00:28:43 +0200

    initial

---
+++ src/js/createCards.js
@@ -0,0 +1,172 @@
+(function createCards() {
+    (function init() {
+        let cardsRow = document.getElementById("cardsRow");
+        let cardsModals = document.getElementById("cardsModals");
+        const cards = getCards().reverse();
+
+        cards.forEach(card => {
+            const color = getColor(card.kcal);
+            const modalId = card.imgSrc.split(".")[0];
+
+            createCard(cardsRow, card, color, modalId);
+            createModal(cardsModals, card, color, modalId);
+        });
+    })();
+
+    function getColor(kcal) {
+        if (kcal < 500) {
+            return "success";
+        } else if (kcal < 700) {
+            return "warning";
+        }
+
+        return "danger";
+    }
+
+    function createCard(cardsRow, card, color, modalId) {
+        let colDiv = document.createElement("DIV");
+        colDiv.setAttribute("class", "col mb-4");
+
+        let cardDiv = document.createElement("DIV");
+        cardDiv.setAttribute("class", "card border-" + color);
+
+        let imgDiv = document.createElement("IMG");
+        imgDiv.setAttribute("src", "src/img/food/" + card.imgSrc);
+        imgDiv.setAttribute("class", "cardImg card-img-top");
+        imgDiv.setAttribute("alt", "...");
+
+        let cardBodyDiv = document.createElement("DIV");
+        cardBodyDiv.setAttribute("class", "card-body");
+
+        let cardBodyH5 = document.createElement("H5");
+        cardBodyH5.setAttribute("class", "card-title");
+        cardBodyH5.textContent = card.title + " ";
+
+        let cardBodyButton = document.createElement("BUTTON");
+        cardBodyButton.setAttribute("type", "button");
+        cardBodyButton.setAttribute("class", "btn btn-outline-" + color);
+        cardBodyButton.setAttribute("data-toggle", "modal");
+        cardBodyButton.setAttribute("data-target", "#modal" + modalId);
+        cardBodyButton.textContent = "Przepis";
+
+        let cardFooterDiv = document.createElement("DIV");
+        cardFooterDiv.setAttribute("class", "card-footer bg-" + color + " text-white");
+        cardFooterDiv.textContent = card.kcal + " kcal";
+
+        cardDiv.appendChild(imgDiv);
+        cardDiv.appendChild(cardBodyDiv);
+        cardBodyH5.appendChild(getKindIcon(card.kind, color));
+        cardBodyDiv.appendChild(cardBodyH5);
+        cardBodyDiv.appendChild(cardBodyButton);
+        cardDiv.appendChild(cardFooterDiv);
+        colDiv.appendChild(cardDiv);
+        cardsRow.appendChild(colDiv);
+    }
+
+    function getKindIcon(kind, color) {
+        let classValue = "fa ";
+
+        if (kind == 0) {
+            classValue += "fa-birthday-cake ";
+        } else if (kind == 1) {
+            classValue += "fa-cutlery ";
+        }
+
+        if (color == "success") {
+            classValue += "green";
+        } else if (color == "warning") {
+            classValue += "yellow";
+        } else if (color == "danger") {
+            classValue += "red";
+        }
+
+        let cardBodyI = document.createElement("I");
+        cardBodyI.setAttribute("class", classValue);
+        cardBodyI.setAttribute("aria-hidden", "true");
+
+        return cardBodyI;
+    }
+
+    function createModal(cardsModals, card, color, modalId) {
+        let modalLabelId = "modal" + modalId + "Label";
+
+        let modalDiv = document.createElement("DIV");
+        modalDiv.setAttribute("class", "modal fade");
+        modalDiv.setAttribute("id", "modal" + modalId);
+        modalDiv.setAttribute("tabindex", "-1");
+        modalDiv.setAttribute("role", "dialog");
+        modalDiv.setAttribute("aria-labelledby", modalLabelId);
+        modalDiv.setAttribute("aria-hidden", "true");
+
+        let modalDialogDiv = document.createElement("DIV");
+        modalDialogDiv.setAttribute("class", "modal-dialog modal-dialog-scrollable modal-dialog-centered modal-lg");
+        modalDialogDiv.setAttribute("role", "document");
+
+        let modalContentDiv = document.createElement("DIV");
+        modalContentDiv.setAttribute("class", "modal-content");
+
+        let modalHeaderDiv = document.createElement("DIV");
+        modalHeaderDiv.setAttribute("class", "modal-header");
+
+        let modalTitleH5 = document.createElement("H5");
+        modalTitleH5.setAttribute("class", "modal-title");
+        modalTitleH5.setAttribute("id", modalLabelId);
+        modalTitleH5.textContent = card.title;
+
+        let modalHeaderButton = document.createElement("BUTTON");
+        modalHeaderButton.setAttribute("type", "button");
+        modalHeaderButton.setAttribute("class", "close");
+        modalHeaderButton.setAttribute("data-dismiss", "modal");
+        modalHeaderButton.setAttribute("aria-label", "Close");
+
+        let modalHeaderButtonSpan = document.createElement("SPAN");
+        modalHeaderButtonSpan.setAttribute("aria-hidden", "true");
+        modalHeaderButtonSpan.textContent = "x";
+
+        let modalBodyDiv = document.createElement("DIV");
+        modalBodyDiv.setAttribute("class", "modal-body");
+
+        let modalListGroupUl = document.createElement("UL");
+        modalListGroupUl.setAttribute("class", "list-group list-group-flush");
+
+        card.products.forEach(product => {
+            let listElementLi = document.createElement("UL");
+            listElementLi.setAttribute("class", "list-group-item");
+            listElementLi.textContent = product;
+
+            modalListGroupUl.appendChild(listElementLi);
+        });
+
+        let modalFooterDiv = document.createElement("DIV");
+        modalFooterDiv.setAttribute("class", "modal-footer");
+
+        let modalFooterAClass = "btn btn-block btn-" + color;
+        let modalFooterATextContent = "Otwórz video tutorial";
+        let isModalFooterAEnabled = !!card.tutorial && card.tutorial != "";
+
+        if (!isModalFooterAEnabled) {
+            modalFooterAClass += " disabled";
+            modalFooterATextContent = "Brak video tutoriala";
+        }
+
+        let modalFooterA = document.createElement("A");
+        modalFooterA.setAttribute("href", card.tutorial);
+        modalFooterA.setAttribute("target", "_blank");
+        modalFooterA.setAttribute("class", modalFooterAClass);
+        modalFooterA.setAttribute("role", "button");
+        modalFooterA.setAttribute("aria-pressed", "true");
+        modalFooterA.textContent = modalFooterATextContent;
+
+        modalHeaderDiv.appendChild(modalTitleH5);
+        modalHeaderDiv.appendChild(modalHeaderButton);
+        modalHeaderButton.appendChild(modalHeaderButtonSpan);
+        modalContentDiv.appendChild(modalHeaderDiv);
+        modalBodyDiv.appendChild(modalListGroupUl);
+        modalContentDiv.appendChild(modalBodyDiv);
+        modalFooterDiv.appendChild(modalFooterA);
+        modalContentDiv.appendChild(modalFooterDiv);
+        modalDialogDiv.appendChild(modalContentDiv);
+        modalDiv.appendChild(modalDialogDiv);
+        cardsModals.appendChild(modalDiv);
+    }
+})();
---
+++ src/js/getCards.js
@@ -0,0 +1,104 @@
+function getCards() {
+    return [{
+        imgSrc: "1.png",
+        title: "Omlet z serka wiejskiego",
+        tutorial: "https://youtu.be/W0zEPbx1wvI?t=59",
+        kcal: 663,
+        kind: 0,
+        products: [
+            "100-150g serka wiejskiego",
+            "2 jajka",
+            "35g płatków owsianych",
+            "pół jabłka",
+            "1 banan",
+            "40g mąki"
+        ]
+    }, {
+        imgSrc: "2.png",
+        title: "Ryżanka z jabłkiem",
+        tutorial: "https://youtu.be/fdtvX-W6Gj8?t=233",
+        kcal: 443,
+        kind: 0,
+        products: [
+            "50g ryżu białego",
+            "1 jabłko",
+            "1 banan",
+            "15g rodzynki",
+            "15g żurawina",
+            "15g orzechy laskowe łuskane"
+        ]
+    }, {
+        imgSrc: "3.png",
+        title: "Ziemniaki z indykiem",
+        tutorial: "https://youtu.be/fdtvX-W6Gj8?t=768",
+        kcal: 478,
+        kind: 1,
+        products: [
+            "300g ziemniaków",
+            "łyżeczka oliwy z oliwek",
+            "120g indyka",
+            "roszponka",
+            "1 średni pomidor",
+            "pół papryki",
+            "Przyprawy do ziemniaków: czosnek granulowany, słodka papryka, sól",
+            "Przyprawy do indyka: słodka papryka, pieprz ziołowy, sól"
+        ]
+    }, {
+        imgSrc: "4.jpg",
+        title: "Makaron w mleczku kokosowym",
+        tutorial: "https://youtu.be/vJLa2ZG-NgI?t=226",
+        kcal: 632,
+        kind: 1,
+        products: [
+            "100g makaronu penne",
+            "5 pieczarek lub 3/4 papryki",
+            "120g indyka",
+            "cebula",
+            "4 łyżki mleczka kokosowego light",
+            "pół ząbka czosnku",
+            "Przyprawy do wszystkiego: sól, pieprz, bazylia, curry (w dużych ilościach)"
+        ]
+    }, {
+        imgSrc: "5.png",
+        title: "Szparagi z makaronem",
+        tutorial: "",
+        kcal: 368,
+        kind: 1,
+        products: [
+            "140g szparagi",
+            "66g pieczarek",
+            "40g cebuli",
+            "100g makaronu",
+            "1 łyżka oliwy z oliwek",
+            "1 ząbek czosnku",
+            "100ml Winiary sos pieczeniowy jasny"
+        ]
+    }, {
+        imgSrc: "6.jpg",
+        title: "Warzywa z tofu",
+        tutorial: "https://youtu.be/fdtvX-W6Gj8?t=453",
+        kcal: 389,
+        kind: 1,
+        products: [
+            "2 łyżki koncentratu pomidorowego",
+            "90g tofu",
+            "100g cukini",
+            "33g cebuli",
+            "70g marchewki",
+            "pół pomidora"
+        ]
+    }, {
+        imgSrc: "7.jpg",
+        title: "Makaron z warzywami",
+        tutorial: "",
+        kcal: 308,
+        kind: 1,
+        products: [
+            "50g makaronu penne",
+            "1 łyżka koncentratu pomidorowego",
+            "30g pomidorów koktajlowych",
+            "5g pestki dyni",
+            "15g słonecznika"
+        ]
+    }];
+};
